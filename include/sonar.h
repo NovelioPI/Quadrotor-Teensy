7e7dc2affa136784d9873f1693a9f733d220e537
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-03-15 16:04:42 +0700

    Change acquisition, using calling function

--- include/sonar.h
+++ include/sonar.h
@@ -2,28 +2,71 @@
 
 #define SONAR_TRIG 1
 #define SONAR_ECHO 0
+#define SOUND_SPEED 344.0f // m/s
 
-float distance, velocity;
-float last_distance = 0;
-const float sonar_offset = 0.0357;
+uint32_t srf_timer = 0;
+float echo_signal = 0;
+
+float alt, alt_rate;
+float prev_alt = 0;
+const float sonar_offset = 5.06;
+uint32_t sonar_time = 0;
+
+void echo_rising();
+void echo_falling();
 
 void sonar_setup() {
-    pinMode(SONAR_TRIG, OUTPUT);
     pinMode(SONAR_ECHO, INPUT);
+    pinMode(SONAR_TRIG, OUTPUT);
+
+    srf_timer = micros();
+
+    attachInterrupt(SONAR_ECHO, echo_rising, RISING);
+
+    digitalWrite(SONAR_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(SONAR_TRIG, HIGH);
+    delayMicroseconds(10);
+
+    alt = (SOUND_SPEED * echo_signal * 0.0001) / 2.0f; // cm
 }
 
 void sonar_loop() {
-    digitalWrite(SONAR_TRIG, HIGH);
-    delayMicroseconds(10);
     digitalWrite(SONAR_TRIG, LOW);
     delayMicroseconds(2);
-    float duration = pulseIn(SONAR_ECHO, HIGH);
-    distance = duration * 0.017; // 0.034 / 2 (m)
-    distance -= sonar_offset;
-    distance = constrain(distance, 0, 400);
+    digitalWrite(SONAR_TRIG, HIGH);
+    delayMicroseconds(10);
+    
+    alt = (SOUND_SPEED * echo_signal * 0.0001) / 2.0f; // cm
+
+    if (abs(alt - prev_alt) < 50) {
+        alt = (alt + prev_alt) / 2;
+    }
+
+    if (alt < 0) {
+        alt = 0;
+    } else if (alt > 200) {
+        alt = 200;
+    }
 
     // Velocity
-    velocity = (distance - last_distance) / 0.01;
+    if (millis() - sonar_time > 100) {
+        sonar_time = millis();
+        alt_rate = (alt - prev_alt) / 0.1f;
+        prev_alt = alt;
+    }
+}
 
-    last_distance = distance;
+void echo_rising()
+{
+  detachInterrupt(SONAR_ECHO);
+  srf_timer = micros();
+  attachInterrupt(SONAR_ECHO, echo_falling, FALLING);
+}
+
+void echo_falling()
+{
+  detachInterrupt(SONAR_ECHO);
+  echo_signal = micros() - srf_timer;
+  attachInterrupt(SONAR_ECHO, echo_rising, RISING);
 }
