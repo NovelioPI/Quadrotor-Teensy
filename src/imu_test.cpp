869cb5d0121b4a8163d0f5194113599da3fa8d62
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-03-15 16:10:39 +0700

    minor change for imu and sonar test

--- src/imu_test.cpp
+++ src/imu_test.cpp
@@ -1,33 +1,30 @@
-#include <Arduino.h>
 #include <imu.h>
 
 #define UART Serial
-#define BAUD_RATE 57600
-
-#define USE_PID
-
-uint32_t loop_timer = 0, debug_timer = 0;
+#define BAUD_RATE 115200
 
 void setup() {
-  UART.begin(BAUD_RATE);
-  delay(250);
+    UART.begin(BAUD_RATE);
+    delay(250);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  imu_setup();
-
-  loop_timer = micros();
+    imu_setup();
 }
 
 void loop() {
-  imu_loop();
+    imu_loop();
 
-  UART.print("Roll:");
-  UART.print(angle_roll);
-  UART.print(" Pitch:");
-  UART.println(angle_pitch);
-
-  if (micros() - loop_timer > 4050) UART.println(micros() - loop_timer);                                      //Turn on the LED if the loop time exceeds 4050us.
-  while (micros() - loop_timer < 4000);                                            //We wait until 4000us are passed.
-  loop_timer = micros();                                                           //Set the timer for the next loop.
+    UART.print("Roll:");
+    UART.print(roll);
+    UART.print(" Pitch:");
+    UART.print(pitch);
+    UART.print(" Yaw:");
+    UART.print(yaw);
+    UART.print(" gyroX:");
+    UART.print(gyroX);
+    UART.print(" gyroY:");
+    UART.print(gyroY);
+    UART.print(" gyroZ:");
+    UART.println(gyroZ);
 }
--- src/sonar_test.cpp
+++ src/sonar_test.cpp
@@ -10,9 +10,9 @@ void setup() {
 void loop() {
     sonar_loop();
     Serial.print("Distance:");
-    Serial.print(distance, 4);
+    Serial.print(alt, 4);
     Serial.print(" Velocity:");
-    Serial.println(velocity, 4);
+    Serial.println(alt_rate, 4);
     
     if (micros() - loop_timer > 4050) Serial.println(micros() - loop_timer);                                      //Turn on the LED if the loop time exceeds 4050us.
     while (micros() - loop_timer < 4000);                                            //We wait until 4000us are passed.
