a01a1ad625b716182f5693b955ff0e06ba6e4d5f
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-04-08 17:12:50 +0700

    Add rolling average filter

--- include/sonar.h
+++ include/sonar.h
@@ -7,12 +7,13 @@
 uint32_t srf_timer = 0;
 float echo_signal = 0;
 
-float alt, alt_rate, alt_rate_slow, alt_rate_fast;
+float alt, alt_slow, alt_fast;
+float alt_rate, alt_rate_slow, alt_rate_fast;
 float prev_alt = 0;
 const float sonar_offset = 5;
 const int num_samples = 20;
-float rotating_avg[num_samples] = {0}, total_avg = 0;
-int rotating_location = 0;
+float rotating_avg[num_samples][2] = {0}, total_avg[2] = {0};
+int rotating_location[2] = {0};
 
 void echo_rising();
 void echo_falling();
@@ -41,28 +42,37 @@ void sonar_loop()
     digitalWrite(SONAR_TRIG, HIGH);
     delayMicroseconds(10);
 
-    alt = (SOUND_SPEED * echo_signal * 0.0001) / 2.0f; // cm
-    alt -= sonar_offset;
+    float alt_temp = (SOUND_SPEED * echo_signal * 0.0001) / 2.0f; // cm
+    alt_temp -= sonar_offset;
 
-    if (alt < 0)
-    {
-        alt = 0;
-    }
-    else if (alt > 200)
-    {
-        alt = 200;
-    }
+    if (abs(alt_temp - prev_alt) > 10) alt_temp = prev_alt;
 
-    // rotating average
-    float alt_rate_temp = (alt - prev_alt) / 0.002f;
+    // rolling average
+    total_avg[0] -= rotating_avg[rotating_location[0]][0];
+    rotating_avg[rotating_location[0]][0] = alt_temp;
+    total_avg[0] += rotating_avg[rotating_location[0]][0];
+    rotating_location[0]++;
+    if (rotating_location[0] >= num_samples) rotating_location[0] = 0;
+    alt_fast = total_avg[0] / (float)num_samples;
+
+    alt_slow = alt_slow * 0.99f + alt_fast * 0.01f;
+    float alt_diff = alt_slow - alt_fast;
+    alt_diff = constrain(alt_diff, -10.0f, 10.0f);
+    if (alt_diff > 1 || alt_diff < -1) alt_slow -= alt_diff;
+    alt = alt_slow;
+
+    alt = constrain(alt, 0.0f, 200.0f);
+
+    // rolling average
+    float alt_rate_temp = (alt - prev_alt) / dt;
     prev_alt = alt;
 
-    total_avg -= rotating_avg[rotating_location];
-    rotating_avg[rotating_location] = alt_rate_temp;
-    total_avg += rotating_avg[rotating_location];
-    rotating_location++;
-    if (rotating_location >= num_samples) rotating_location = 0;
-    alt_rate_fast = total_avg / (float)num_samples;
+    total_avg[1] -= rotating_avg[rotating_location[1]][1];
+    rotating_avg[rotating_location[1]][1] = alt_rate_temp;
+    total_avg[1] += rotating_avg[rotating_location[1]][1];
+    rotating_location[1]++;
+    if (rotating_location[1] >= num_samples) rotating_location[1] = 0;
+    alt_rate_fast = total_avg[1] / (float)num_samples;
 
     alt_rate_slow = alt_rate_slow * 0.98f + alt_rate_fast * 0.02f;
     alt_rate = alt_rate_slow;
